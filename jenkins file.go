78fd7db26676488622237046f5c220212433e758
muruganmuglian <144046927+muruganmuglian@users.noreply.github.com>
2023-12-24 00:39:42 +0530

    Add files via upload

---
+++ jenkins file.go
@@ -0,0 +1,28 @@
+pipeline{
+	agent any
+
+	stages{
+		stage('build'){
+			steps{
+				sh 'maven clan package'
+		
+	}
+	stage ( 'deploy to tomcat server'){
+		post{
+			success
+			{
+				echo "archiving the artifacts"
+				archivingArtifacts artifacts: '**war/target/*.war'
+			}
+		}
+	}
+stage ('deploy to tomcat server'){
+	steps{
+		deploy adapters: deploy adapters: deploy adapters: [tomcat8(credentialsId: 'T', path: '', url: 'http://localhost:9090')], contextPath: null, war: '**/*.war'
+	}
+
+
+}
+		}
+	}
+}
